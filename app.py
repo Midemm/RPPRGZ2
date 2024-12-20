fc5233093466c84a95f18858e21590153af8253b
Колесников Дмитрий <Kolesn2003@yandex.ru>
2024-12-21 06:41:51 +0700

    3

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from models import db, User, Resource
 import datetime
